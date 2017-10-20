1a07361547cca2b959fdcfc7efd52093077cba73
Kent Gruber <kgruber1@emich.edu>
2017-10-20 13:11:54 -0400

    Add client path to require code and method shortcut.

--- lib/shodanz.rb
+++ lib/shodanz.rb
@@ -2,9 +2,13 @@ require "oj"
 require "unirest"
 require "shodanz/version"
 require "shodanz/api"
+require "shodanz/client"
 
 module Shodanz
   def self.api
     API
+  end
+  def self.client
+    Client
   end
 end
