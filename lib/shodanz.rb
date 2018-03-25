3efe90ddfc19f961a48fa56ca00ef54d3230822d
Kent Gruber <kgruber1@emich.edu>
2018-03-25 12:14:42 -0400

    some minor rubocop shenanigans

--- lib/shodanz.rb
+++ lib/shodanz.rb
@@ -1,13 +1,18 @@
-require "unirest"
-require "oj"
-require "shodanz/version"
-require "shodanz/api"
-require "shodanz/client"
+require 'unirest'
+require 'oj'
+require 'shodanz/version'
+require 'shodanz/api'
+require 'shodanz/client'
 
+# Shodanz is a modern Ruby gem for Shodan, the world's
+# first search engine for Internet-connected devices.
 module Shodanz
+  # Shortcut for {Shodanz::API}
   def self.api
     API
   end
+
+  # Shortcut for {Shodanz::Client}
   def self.client
     Client
   end
