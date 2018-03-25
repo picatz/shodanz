5f7c3fc5d9cbc2b51558a65eed19b6009961cedf
Kent Gruber <kgruber1@emich.edu>
2018-03-25 12:15:09 -0400

    some minor rubocop shenanigans

--- lib/shodanz/client.rb
+++ lib/shodanz/client.rb
@@ -1,22 +1,22 @@
 module Shodanz
+  # General client container class for all three
+  # of Shodan's available API endpoints in a
+  # convient place to use.
+  #
+  # @author Kent 'picat' Gruber
   class Client
+    # @return [Shodanz::API::REST]
+    attr_reader :rest_api
+    # @return [Shodanz::API::Streaming]
+    attr_reader :streaming_api
+    # @return [Shodanz::API::Exploits]
+    attr_reader :exploits_api
+
     # Create a new client to connect to any of the APIs.
     def initialize
       @rest_api      = Shodanz.api.rest.new
       @streaming_api = Shodanz.api.streaming.new
-      @exploits_api  = Shodanz.api.exploits.new  
-    end
-
-    def rest_api
-      @rest_api
-    end
-
-    def streaming_api
-      @streaming_api
-    end
-
-    def exploits_api
-      @exploits_api
+      @exploits_api  = Shodanz.api.exploits.new
     end
   end
 end
