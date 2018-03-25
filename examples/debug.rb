fc45d7748515dc0d7c971fef0b4087a30db68b61
Kent Gruber <kgruber1@emich.edu>
2018-03-25 11:37:43 -0400

    minor debug example update

--- examples/debug.rb
+++ examples/debug.rb
@@ -2,8 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'shodanz'
 require 'pry'
 
-rest_api      = Shodanz.api.rest.new
-streaming_api = Shodanz.api.streaming.new
-exploits_api  = Shodanz.api.exploits.new
+# rest_api      = Shodanz.api.rest.new
+# streaming_api = Shodanz.api.streaming.new
+# exploits_api  = Shodanz.api.exploits.new
+
+client = Shodanz.client.new
 
 binding.pry
