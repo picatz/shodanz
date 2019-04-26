a3fb46036257ce8e8aabe7106001cd4a95414448
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-26 13:58:13 -0400

    Add basic .info check

--- spec/rest_api_spec.rb
+++ spec/rest_api_spec.rb
@@ -5,5 +5,30 @@ RSpec.describe Shodanz::API::REST do
     @client = Shodanz.api.rest.new
   end
 
+  describe '.info' do
+    def check
+      if Async::Task.current?
+        resp = @client.info.result
+      else
+        resp = @client.info
+      end
+      expect(resp).to be_a(Hash)
+    end
+
+    context 'syncronously' do
+      it 'returns info about the underlying token' do
+        check
+      end
+    end
+
+    context 'asyncronously' do
+      it 'returns info about the underlying token' do
+        Async do
+          check
+        end
+      end
+    end
+  end
+
 
 end
