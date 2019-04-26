e9ded2a1271528ce034e4645daae8e1333f38948
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-26 13:58:21 -0400

    Add basic .host check

--- spec/rest_api_spec.rb
+++ spec/rest_api_spec.rb
@@ -30,5 +30,32 @@ RSpec.describe Shodanz::API::REST do
     end
   end
 
+  describe '.host' do
+    let(:ip) { "8.8.8.8" }
+
+    def check
+      if Async::Task.current?
+        resp = @client.host(ip).result
+      else
+        resp = @client.host(ip)
+      end
+      expect(resp).to be_a(Hash)
+    end
+
+    context 'syncronously' do
+      it 'returns all services that have been found on the given host IP' do
+        check
+      end
+    end
+
+    context 'asyncronously' do
+      it 'returns all services that have been found on the given host IP' do
+        Async do
+          check
+        end
+      end
+    end
+  end
+  
 
 end
