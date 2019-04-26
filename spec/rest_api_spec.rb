1d2c4c97fefe1c20ab654535e5d300770c3e0f42
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-26 16:58:12 -0400

    Add basic .host_search_tokens check

--- spec/rest_api_spec.rb
+++ spec/rest_api_spec.rb
@@ -103,4 +103,34 @@ RSpec.describe Shodanz::API::REST do
     end
   end
 
+  describe '#host_search_tokens' do
+    let(:query) { "apache" }
+
+    def check
+      if Async::Task.current?
+        resp = @client.host_search_tokens(query).wait
+      else
+        resp = @client.host_search_tokens(query)
+      end
+      expect(resp).to be_a(Hash)
+      expect(resp['attributes']).to be_a(Hash)
+      expect(resp['errors']).to be_a(Array)
+      expect(resp['filters']).to be_a(Array)
+      expect(resp['string']).to be_a(String)
+      expect(resp['string']).to eq(query)
+    end
+
+    describe 'returns the total number of results that matches a given query' do
+      it 'works synchronously' do
+        check
+      end
+
+      it 'works asynchronously' do
+        Async do
+          check
+        end
+      end
+    end
+  end
+
 end
