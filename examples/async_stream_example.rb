0f5d44d1e042fb111a7b3b0904e4658123b6e0fe
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-28 21:36:31 -0400

    Update async_stream_example.rb

--- examples/async_stream_example.rb
+++ examples/async_stream_example.rb
@@ -1,8 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'pry'
 require 'async'
 require 'shodanz'
-
-`clear` # clear the screen
 
 client = Shodanz.client.new
 
@@ -13,11 +12,22 @@ services = ['ftp', 'ssh', 'http', 'https']
 
 ports_with_service_names = ports.zip(services)
 
-Async do
+Async do 
   # collect banners for ports
   ports_with_service_names.each do |port, service|
-    client.streaming_api.banners_on_port(port) do |_|
-      puts "#{stats[port] += 1} #{service}"
+    client.streaming_api.banners_on_port(port) do |banner|
+      if ip = banner['ip_str']
+        Async do
+          resp = client.rest_api.honeypot_score(ip).wait
+          binding.pry if resp.nil?
+          puts "#{ip} has a #{resp *100}% chance of being a honeypot"
+        rescue Shodanz::Errors::RateLimited
+          sleep 1
+          retry
+        rescue => error
+          binding.pry
+        end
+      end
     end
   end
 end
