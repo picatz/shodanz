a33ab7e059b0bb832ba2880132eeecd9c698ab5e
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-28 22:41:16 -0400

    Add basic .banner check
    with limit

--- spec/streaming_api_spec.rb
+++ spec/streaming_api_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Shodanz::API::Streaming do
     @client = Shodanz.api.streaming.new
   end
 
+  describe '#banners' do
+    def check
+      @client.banners(limit: 1) do |banner|
+        expect(banner).to be_a(Hash)
+      end
+    end
+
+    describe 'stream any banner data Shodan collects' do
+      it 'works synchronously' do
+        check
+      end
+
+      it 'works asynchronously' do
+        Async do
+          check
+        end
+      end
+    end
+  end
 end
