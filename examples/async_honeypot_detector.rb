d225a6a5defd17e5fdc3da00015ce6e0d784ee75
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-01 13:28:39 -0400

    Update async_honeypot_detector.rb

--- examples/async_honeypot_detector.rb
+++ examples/async_honeypot_detector.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'async'
 require 'shodanz'
 
@@ -7,11 +7,10 @@ client = Shodanz.client.new
 client.streaming_api.banners do |banner|
   if ip = banner['ip_str']
     Async do
-      if score = client.rest_api.honeypot_score(ip).wait
-        puts "#{ip} has a #{score*100}% chance of being a honeypot"
-      end
+      score = client.rest_api.honeypot_score(ip).wait
+      puts "#{ip} has a #{score * 100}% chance of being a honeypot"
     rescue Shodanz::Errors::RateLimited
-      sleep rand 
+      sleep rand
       retry
     rescue # any other errors
       next
