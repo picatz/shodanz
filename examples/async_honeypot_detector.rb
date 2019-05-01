32ff81383631eb53bf61e17500159508076acc3a
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-01 12:07:29 -0400

    Update async_honeypot_detector.rb

--- examples/async_honeypot_detector.rb
+++ examples/async_honeypot_detector.rb
@@ -8,12 +8,12 @@ client.streaming_api.banners do |banner|
   if ip = banner['ip_str']
     Async do
       if score = client.rest_api.honeypot_score(ip).wait
-        puts "#{ip} has a #{score *100}% chance of being a honeypot"
+        puts "#{ip} has a #{score*100}% chance of being a honeypot"
       end
     rescue Shodanz::Errors::RateLimited
       sleep rand 
       retry
-    rescue EOFError, Shodanz::Errors::NoInformation
+    rescue # any other errors
       next
     end
   end
