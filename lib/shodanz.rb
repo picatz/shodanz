cb1c0e0669c2c37b314edbc81c6026f936cd9be7
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-01 13:28:44 -0400

    Update shodanz.rb

--- lib/shodanz.rb
+++ lib/shodanz.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'async'
 require 'async/http/internet'
@@ -5,6 +7,9 @@ require 'shodanz/version'
 require 'shodanz/errors'
 require 'shodanz/api'
 require 'shodanz/client'
+
+# disable async logs by default
+Async.logger.level = 4
 
 # Shodanz is a modern Ruby gem for Shodan, the world's
 # first search engine for Internet-connected devices.
