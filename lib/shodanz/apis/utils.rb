2e1f2c7dfb5f745381a17a30fa782c827e49242a
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-30 12:11:38 -0400

    Update utils.rb
    Now only stuff relevant to Exploits and REST exist

--- lib/shodanz/apis/utils.rb
+++ lib/shodanz/apis/utils.rb
@@ -2,68 +2,11 @@
 
 module Shodanz
   module API
-    # Utils provides simply get, post, and slurp_stream functionality
-    # to the client. Under the hood they support both async and non-async
-    # usage. You should basically never need to use these methods directly.
+    # Utils provides some common methods used in the REST
+    # and Exploits API. You shouldn't need to use this module directly.
     #
     # @author Kent 'picat' Gruber
     module Utils
-      # Response ensures the parsed JSON body doesn't use symbols for key names.
-      class Response < Async::REST::Representation
-        class Parser < Protocol::HTTP::Body::Wrapper
-          def join
-            ::JSON.parse(super, symbolize_names: false)
-          end
-        end
-
-        def process_response(request, response)
-          if body = response.body
-            response.body = Parser.new(body)
-          end
-
-          return response
-        end
-      end
-
-      # Perform a direct GET HTTP request to the REST API.
-      def get(path, **params)
-        params = params.transform_keys(&:to_sym)
-
-        params[:key] = @key
-
-        task = Async::REST::Resource.for(@url) do |resource|
-          handle_any_json_errors(resource.with(path: path, parameters: params).get(Response).value)
-        end
-
-        task.wait unless Async::Task.current?
-      end
-
-      # Perform a direct POST HTTP request to the REST API.
-      def post(path, **params)
-        params = params.transform_keys(&:to_sym)
-
-        params[:key] = @key
-
-        task = Async::REST::Resource.for(@url) do |resource|
-          handle_any_json_errors(resource.with(path: path, parameters: params).post(Response).value)
-        end
-
-        task.wait unless Async::Task.current?
-      end
-
-      # Perform the main function of consuming the streaming API.
-      def slurp_stream(path, **params)
-        if Async::Task.current?
-          async_slurp_stream(path, params) do |result|
-            yield result
-          end
-        else
-          sync_slurp_stream(path, params) do |result|
-            yield result
-          end
-        end
-      end
-
       def turn_into_query(params)
         filters = params.reject { |key, _| key == :query }
         filters.each do |key, value|
@@ -84,57 +27,24 @@ module Shodanz
         facets.select { |key, _| key == :facets }
       end
 
-      private
+      def async(&block)
+        if task = Async::Task.current?
+          yield
+        else
+          Async do
+            result = yield
 
-      RATELIMIT = 'rate limit reached'
-      NOINFO    = 'no information available'
-      NOQUERY   = 'empty search query'
+            self.close
 
-      def handle_any_json_errors(json)
-        return json unless json.is_a?(Hash) && json.key?('error')
-
-        raise Shodanz::Errors::RateLimited if json['error'].casecmp(RATELIMIT) >= 0
-        raise Shodanz::Errors::NoInformation if json['error'].casecmp(NOINFO) >= 0
-        raise Shodanz::Errors::NoQuery if json['error'].casecmp(NOQUERY) >= 0
-
-        json
-      end
-
-      def slurper(path, **params)
-        # param keys should all be strings
-        params = params.transform_keys(&:to_s)
-        # check if limit
-        if (limit = params.delete('limit'))
-          counter = 0
-        end
-        # make GET request to server
-        resp = @internet.get("#{@url}#{path}?key=#{@key}", params)
-        # read body line-by-line
-        until resp.body.nil? || resp.body.empty?
-          resp.body.read.each_line do |line|
-            next if line.strip.empty?
-
-            yield JSON.parse(line)
-            if limit
-              counter += 1
-              resp.close if counter == limit
-            end
-          end
-        end
-      ensure
-        resp&.close
-      end
-
-      def async_slurp_stream(path, **params)
-        Async::Task.current.async do
-          slurper(path, params) { |data| yield data }
+            result
+          end.wait
         end
       end
 
-      def sync_slurp_stream(path, **params)
-        Async do
-          slurper(path, params) { |data| yield data }
-        end.wait
+      def get_value(path, parameters = nil)
+        async do
+          Representation.new(@resource.with(path: path, parameters: parameters), wrapper: @wrapper).value
+        end
       end
     end
   end
