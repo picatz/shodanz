5ffc4be5594b57299ed0bbdb71553d742e39b72d
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-31 11:56:20 -0400

    Update spec_helper.rb
    Used rubocop auto-correct

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
-require "bundler/setup"
-require "async/rspec"
-require "shodanz"
-require "pry"
-require "readline"
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'async/rspec'
+require 'shodanz'
+require 'pry'
+require 'readline'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
