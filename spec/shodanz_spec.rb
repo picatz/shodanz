c618d1c6aa10dff6f533a50ca810b2419c5d5024
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-05-31 11:56:49 -0400

    Update shodanz_spec.rb
    Used rubocop auto-correct

--- spec/shodanz_spec.rb
+++ spec/shodanz_spec.rb
@@ -1,7 +1,9 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 RSpec.describe Shodanz do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Shodanz::VERSION).not_to be nil
   end
 end
